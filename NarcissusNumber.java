6bdbfc579901bf91da0e65023ca9825c1254dd51
xiaoyang <lixiaoyang2020@gmail.com>
2017-08-09 09:19:09 +0800

    Update NarcissusNumber.java

--- NarcissusNumber.java
+++ NarcissusNumber.java
@@ -1,14 +1,17 @@
-
+/*
+Java实现水仙花数
+*/
 public class NarcissusNumber {
 	public static void main(String[] args) {
 		int a=0;
 		int b=0;
 		int c=0;
 		for(int i=100;i<999;i++){
-			a=i/100;
-			b=i/10%10;
-			c=i%10;
-		
+			a=i/100;	//求出百位数
+			b=i/10%10;	//求出十位数
+			c=i%10;		//求出个位数
+			
+			//这么写显然不是好的写法，Math.pow(a,3)可以计算a的3次方
 			if (i==(a*a*a+b*b*b+c*c*c)) {
 				System.out.println(i);
 			}
