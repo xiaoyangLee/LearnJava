2d1d63f2eeed0240d29fa5076f80e2d8cc3f2710
xiaoyangLee <lixiaoyang2020@gmail.com>
2017-09-24 07:56:07 +0800

    update

---
+++ N9Multiplication.java
@@ -0,0 +1,11 @@
+
+public class N9Multiplication {
+	public static void main(String[] args) {
+		for(int row=1;row<10;row++){
+			for(int col=1;col<=row;col++){
+				System.out.println(row+"*"+col+"="+row*col+"");
+				System.out.println(" ");
+			}
+		}
+	}
+}
